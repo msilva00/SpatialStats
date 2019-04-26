5c5e9370bd15a18be5b45c8d4a1230973c85f2b7
marysilva <msilva2@mail.sfsu.edu>
2019-04-26 04:40:53 -0700

    add to function library

--- FunctionLibrary.R
+++ FunctionLibrary.R
@@ -71,3 +71,14 @@ eigen_approx <- function(d, J=100, L=1) {
   #Re(out)
   Re(out)
 }
+
+gp_sim <- function(x, mu=0, cov_fn, eps=1E-10) {
+  n <- length(x)
+  Sig <- cov_fn(as.matrix(dist(x)) + diag(eps,n))
+  
+  #print(Sig)
+  
+  t_chol_S <- t(chol(Sig))
+  
+  mu + t_chol_S %*% rnorm(n)
+}
